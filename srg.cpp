81e91cf24c75f8c3d91dadfa236ab2585c678c1a
Ryan Jenkins <rjenkins@mail.sfsu.edu>
2015-03-11 16:12:13 -0700

    typedefs and stuff.

--- srg.cpp
+++ srg.cpp
@@ -1,6 +1,39 @@
 #include "itkGDCMImageIO.h"
 
+typedef itk::Image<short, 3> ImageType;
+typedef itk::ImageSeriesReader<ImageType> ReaderType;
+typedef itk::GDCMImageIO ImageIOType;
+typedef itk::GDCMSeriesFileNames InputNamesGeneratorType;
+typedef itk::NumericSeriesFileNames OutputNamesGeneratorType;
+
+void print_usage(argv) {
+  fprintf(stderr, "%s dicom_series", argv[0]);
+}
+
 int main(int argc, char* argv[]) {
-  printf("Hellow world\n");
+  if (argc < 2) {
+    print_usage(argv);
+    return 1;
+  }
+
+  ImageIOType::Pointer gdcmIO = ImageIOType::New();
+
+  InputNamesGeneratorType::Pointer inputNames = InputNamesGeneratorType::New();
+  inputNames->SetInputDirectory(argv[1]);
+
+  const ReaderType::FileNamesContainer & filenames =
+    inputNames->GetInputFileNames();
+ 
+  ReaderType::Pointer reader = ReaderType::New();
+ 
+  reader->SetImageIO(gdcmIO);
+  reader->SetFileNames(filenames);
+  try {
+    reader->Update();
+  } catch (itk::ExceptionObject &excp) {
+    fprintf(stderr, "Exception thrown while reading the series\n");
+    return 1;
+  }
+
   return 0;
 }
