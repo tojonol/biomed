87cef5974163e928b4edc901561f269d4e75cd0c
RyanJenkins <rjenkins@mail.sfsu.edu>
2015-04-17 15:15:29 -0700

    Seperated headers into `include` tree and added MCMain build target.

---
+++ src/MeshConstruction/MeshConstruction.cxx
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "itkMesh.h"
+#include "itkMeshFileWriter.h"
+
+#include "DICOMImage.h"
+
+typedef itk::Mesh<PixelType, 3>  MeshType;
+
+int main() {
+  printf("sup\n");
+}
