b8c7f53cede0ed147312590f91b6b13b3e2abaee
RyanJenkins <rjenkins@mail.sfsu.edu>
2015-04-25 14:09:18 -0700

    Added region seeds UI to IJ seed picker.

--- SegGUI/SeedPicker.java
+++ SegGUI/SeedPicker.java
@@ -1,24 +1,104 @@
-// Prototype plugin tool. There are more plugin tools at
-// http://imagej.nih.gov/ij/plugins/index.html#tools
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.BoxLayout;
+
 import ij.*;
 import ij.process.*;
 import ij.gui.*;
-import java.awt.*;
 import ij.plugin.tool.PlugInTool;
-import java.awt.event.*;
+
+class Point {
+  int x, y, slice;
+
+  Point(int x, int y, int slice) {
+    this.x = x;
+    this.y = y;
+    this.slice = slice;
+  }
+
+  public String toString() {
+    return String.format("(%d, %d, %d)", this.x, this.y, this.slice);
+  }
+}
+
+class AddRegionListener implements ActionListener {
+  SeedsViewer viewer;
+
+  AddRegionListener(SeedsViewer viewer) {
+    this.viewer = viewer;
+  }
+
+  public void actionPerformed(ActionEvent e) {     
+    this.viewer.addRegion();
+  }
+}
+
+class SeedsViewer {
+  private static SeedsViewer instance;
+  static int regionNo;
+
+  Frame mainFrame;
+  java.awt.List regionList;
+  java.awt.List pointsList;
+  HashMap<String, ArrayList<Point>> regionSeeds;
+
+  private SeedsViewer() {
+    this.regionSeeds = new HashMap<String, ArrayList<Point>>();
+
+    this.mainFrame = new Frame("Region Seeds");
+    this.mainFrame.setSize(500, 300);
+    this.mainFrame.setLayout(new BoxLayout(this.mainFrame, BoxLayout.X_AXIS));
+
+    Container leftContainer = new Container();
+    Container rightContainer = new Container();
+    leftContainer.setLayout(new BoxLayout(leftContainer, BoxLayout.Y_AXIS));
+    rightContainer.setLayout(new BoxLayout(rightContainer, BoxLayout.Y_AXIS));
+
+    this.regionList = new java.awt.List(1);
+    this.pointsList = new java.awt.List(1);
+
+    Button addRegionButton = new Button("New Region");
+    addRegionButton.addActionListener(new AddRegionListener(this));
+
+    leftContainer.add(this.regionList);
+    leftContainer.add(addRegionButton);
+    rightContainer.add(this.pointsList);
+
+    this.mainFrame.add(leftContainer);
+    this.mainFrame.add(rightContainer);
+
+    this.mainFrame.setVisible(true);  
+    IJ.log("LOLLZ");
+  }
+
+  public static SeedsViewer getInstance() {
+    if (instance == null)
+      instance = new SeedsViewer();
+
+    return instance;
+  }
+
+  public void addRegion() {
+    String name = "Region-" + regionNo++;
+    this.regionSeeds.put(name, new ArrayList<Point>());
+    this.regionList.add(name);
+  }
+}
 
 public class SeedPicker extends PlugInTool {
 
 	public void mousePressed(ImagePlus imp, MouseEvent e) {
+    Point point = new Point(e.getX(), e.getY(), imp.getCurrentSlice());
 
-    IJ.log(String.format("click: (%d, %d, %d)", e.getX(), e.getY(), imp.getCurrentSlice()));
-	}
+    IJ.log("click: " + point.toString());
 
-	public void mouseDragged(ImagePlus imp, MouseEvent e) {
-		IJ.log("mouse dragged: "+e);
+    SeedsViewer.getInstance();
 	}
 
 	public void showOptionsDialog() {
 		IJ.log("icon double-clicked");
 	}
 }
+
+
